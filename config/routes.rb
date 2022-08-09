ea9fae58abb180c001c5512b04101dfded08bb5c
YK2022-July <105860045+YK2022-July@users.noreply.github.com>
2022-08-09 09:02:06 +0000

    [Edit]3-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'books/index'
   post 'books/index' => 'books#create'
-  get 'books/show'
-  get 'books/edit'
+  get 'books/show/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
